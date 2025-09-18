504b3453a025acc182b1141beeb7a1ba897ab28c
rajusiripalli <rajusiripalli7@gmail.com>
2025-09-18 11:34:58 +0530

    post details UI updated 2hrs: 10 mins

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,5 +1,7 @@
-import { Redirect, Stack } from 'expo-router'
+import { View } from 'react-native'
+import { Redirect, Stack, router } from 'expo-router'
 import { useAuth } from '@clerk/clerk-expo'
+import { AntDesign, Entypo, MaterialIcons } from '@expo/vector-icons';
 
 export default function AppLayout() {
     const { isSignedIn } = useAuth()
@@ -11,6 +13,20 @@ export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="post/[id]" options={{ 
+        headerTitle: '',
+        headerStyle: {backgroundColor: '#FF5700'},
+        headerLeft: ()=> <AntDesign name="arrowleft" size={24} color="white" onPress={()=> router.back()} />,
+        headerRight: () => 
+          <View style={{flexDirection: 'row', gap: 10,}}>
+            <AntDesign name="search1" size={24} color="white"  />
+            <MaterialIcons name="sort" size={27} color="white"  />
+            <Entypo name="dots-three-horizontal" size={27} color="white"  />
+
+          </View>,
+        animation: 'slide_from_bottom',
+        }} 
+      />
     </Stack>
   )
 }
